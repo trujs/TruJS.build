808a3cf0f33b3859d883b3b211d9bb02198a603a
rmcinto <rodger@geekshake.com>
2020-01-28 20:06:17 -0700

    BootstrpFormatter: added recursion for object value updates

--- handlers/bootstrap/_BootstrapFormatter.js
+++ handlers/bootstrap/_BootstrapFormatter.js
@@ -22,25 +22,10 @@ function _BootstrapFormatter (
         , assets
     ) {
         try {
-            var data = assets[0].data
-            , templateDefaults = entry.templateDefaults;
-
-            data = data.replace(VAR_PATT, function replaceVars(match, name) {
-                var ref = utils_reference(name, entry)
-                , val = "";
-                if (ref.found && ref.value !== undefined) {
-                    val = ref.value;
-                }
-                else if (templateDefaults.hasOwnProperty(name)) {
-                    val = templateDefaults[name];
-                }
-                if (typeof val === "object") {
-                    val = JSON.stringify(val);
-                }
-                return val;
-            });
-
-            assets[0].data = data;
+            assets[0].data = updateValue(
+                assets[0].data
+                , entry
+            );
 
             return promise.resolve(assets);
         }
@@ -48,4 +33,31 @@ function _BootstrapFormatter (
             return promise.reject(ex);
         }
     };
+
+    /**
+    * @function
+    */
+    function updateValue(value, entry) {
+        return value.replace(VAR_PATT, function replaceVars(match, name) {
+            var ref = utils_reference(name, entry)
+            , val = "";
+            if (ref.found && ref.value !== undefined) {
+                val = ref.value;
+            }
+            else if (entry.templateDefaults.hasOwnProperty(name)) {
+                val = entry.templateDefaults[name];
+                if (typeof val === "object") {
+                    val = JSON.stringify(val);
+                }
+                val = updateValue(
+                    val + ""
+                    , entry
+                );
+            }
+            if (typeof val === "object") {
+                val = JSON.stringify(val);
+            }
+            return val;
+        });
+    }
 }
